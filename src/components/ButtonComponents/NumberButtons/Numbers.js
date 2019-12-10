21ad409bdcbfefc81b255f4579f54d32aa4da1e8
mgroff1 <mick.groff@gmail.com>
2019-12-10 06:33:07 -0700

    -almost-
    -almost-

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,19 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
+import {numbers} from '../../../data';
+import NumberButton from './NumberButton';
 
-//import any components needed
-// example of import from data.js. Note all the ../   This is how we move through folders. 
-/* 
-import { numbers } from '../../../data' 
-*/
-//Import your array data to from the provided data file
+function Numbers (props) {
 
-const Numbers = () => {
-  // STEP 2 - add the imported data to state
+  const [numberBtn] = useState(numbers);
+
   return (
-    <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
-    </div>
+<div className = "num-buttons">
+      {numberBtn.map ((item, index) =>
+        <NumberButton key={index} setDisplay={props.setDisplay} display={props.displayResult} text={item} />
+      )}
+</div>
   );
 };
+
+export default Numbers;
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
+import {operators} from '../../../data';
+import OperatorButton from './OperatorButton'
 
-//import any components needed
 
-//Import your array data to from the provided data file
+function Operators(props){
+  const [operatorBtn] = useState(operators);
 
-const Operators = () => {
-  // STEP 2 - add the imported data to state
   return (
-    <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+    <div className = "opp-buttons">
+      {operatorBtn.map((item, index) =>
+        <OperatorButton key={index} setDisplay={props.setDisplay} display={props.displayResult} text={item.char} />
+      )}
+
     </div>
   );
 };
+
+export default Operators;
